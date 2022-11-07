ef66f28bb56fbb1d66ab69ee50c0d7d7a7af4da2
HASSAN <hassan.aburesha@gmail.com>
2022-11-07 16:31:17 +0100

    .this exercises

--- week-2/constructor.js
+++ week-2/constructor.js
@@ -9,22 +9,40 @@
 
 // // prototype:
 
-function rectangle(width, height) {
-    this.width = width;
-    this.height = height;
+function Rectangle(w, h) {
+    this.width = w;
+    this.height = h;
 
-    function getArea(rectangle) {
+    this.getArea = function () {
         return this.width * this.height;
-        console.log("The Area is: ", getArea);
-    }
+    };
 }
 
-let rectangleOne = new rectangle(2, 4);
+const rectangle = new Rectangle(3, 4);
+console.log(rectangle);
+console.log("area: ", rectangle.getArea());
 
-// var rectangleOne = new rectangle(1, 2);
+// let rectangleOne = new rectangle(2, 4);
 // console.log(rectangleOne);
-// getArea(rectangleOne);
 
-// function square(lengthAndWidth) {
+// rectangleOne.getArea();
+// console.log(rectangleOne.getArea());
 
+// console.log(typeof rectangleOne);
+
+// function square(width) {
+//     this.width = width;
+//     this.height = width;
 // }
+
+// let squareOne = new square(6);
+
+// squareOne.getArea();
+// console.log(squareOne.getArea());
+
+// // function square(widthAndHeight) {
+// //     this.widthAndHeight = widthAndHeight;
+// //     this.getArea = function () {
+// //         return this.widthAndHeight * 2;
+// //     };
+// // }
