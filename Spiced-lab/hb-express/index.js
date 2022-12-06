f861fb6446c874067eb8346458abd1a0e37d0306
HASSAN <hassan.aburesha@gmail.com>
2022-12-06 10:35:02 +0100

    hb-describtion page

--- Spiced-lab/hb-express/index.js
+++ Spiced-lab/hb-express/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Handlebars Setup
 const { engine } = require("express-handlebars");
-const projects = require("./projects.json");
+const projectsList = require("./projects.json");
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
@@ -14,24 +14,40 @@ app.set("view engine", "handlebars");
 app.use(express.static("./public"));
 app.use(express.static("./projects"));
 
-const cohortName = "Mint";
-
 app.get("/", (req, res) => {
     res.render("home", {
         //whatever you specify here,
         // it will be used as a body in the main.handlebars!
         // (i.e. home.handlebars)
         layout: "main",
-        cohortName,
-        projects,
+        projects: projectsList,
+        showImage: true,
+        helpers: {
+            getnewstyle: "/style.css",
+        },
     });
 });
 
-app.get("/about", (req, res) => {
-    res.render("aboutTemplate", {
-        teachers,
-        cohortName,
+app.get("/projects/:projectDirectory", (req, res) => {
+    const projectDirectory = "/" + req.params.projectDirectory; // 'kitty-caroussel';
+    const selectedProject = projectsList.find((p) => {
+        console.log("projectdirectory", projectDirectory);
+        return p.path === projectDirectory;
     });
+    console.log(selectedProject);
+    if (selectedProject === undefined) {
+        res.statusCode = 404;
+        res.end("PAGE NOT FOUND");
+    } else
+        res.render("home", {
+            layout: "main",
+            projects: projectsList,
+            showImage: false,
+            selectedProject: selectedProject,
+            helpers: {
+                getnewstyle: "/newstyle.css",
+            },
+        });
 });
 
 app.listen(3000, console.log("running at 3000"));
